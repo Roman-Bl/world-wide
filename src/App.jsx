c158539493e5e7e8a65ac2df9a39b6fdc81d35fd
Roman Blinov <romeo.blinov@gmail.com>
2023-08-22 18:52:22 +0300

    static layout finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
-        <Route path="product" element={<Product />}></Route>
-        <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="/app" element={<AppLayout />}></Route>
+        <Route path="/product" element={<Product />}></Route>
+        <Route path="/pricing" element={<Pricing />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </BrowserRouter>
