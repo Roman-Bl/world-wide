353913de105e67825109ef694b66aa5128194dd5
Roman Blinov <romeo.blinov@gmail.com>
2023-08-22 18:03:17 +0300

    project starter files done

---
+++ src/pages/Product.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function Product() {
+  return (
+    <div>
+      <PageNav />
+      <h3>Product</h3>
+    </div>
+  );
+}
+
+export default Product;
